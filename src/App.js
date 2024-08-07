38ef7c974c64d79652b3bded3f18185614421f53
taha <taahamhd@gmail.com>
2024-08-07 17:32:18 +0100

    added file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        ciao there.
+        hello there.
         </p>
         <a
           className="App-link"
